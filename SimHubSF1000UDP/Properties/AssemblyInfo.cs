d36733cfc0d39f752b5403e83c9956f9a9b80ec3
ducng99 <tom@ducng.dev>
2023-12-29 19:19:57 +1300

    Updated app name

--- SimHubSF1000UDP/Properties/AssemblyInfo.cs
+++ SimHubSF1000UDP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("SimHubSF1000UDP")]
+[assembly: AssemblyTitle("SimHub SF1000 UDP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimHubSF1000UDP")]
+[assembly: AssemblyProduct("SimHub SF1000 UDP")]
 [assembly: AssemblyCopyright("Copyright ©  2023 Thomas Nguyen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
